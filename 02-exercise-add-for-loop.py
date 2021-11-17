051e8ab8367fe575993e3f4b77d8c36c8433ad78
mdAshrafuddin <mdtanjil01753511918@gmail.com>
2021-11-17 21:53:32 +0600

    math fomula sum

--- 02-exercise-add-for-loop.py
+++ 02-exercise-add-for-loop.py
@@ -4,4 +4,11 @@ sum = 0
 
 for num in range(0, n+1):
     sum = sum + num
-print("SUM of first {0} numbers is {1}".format(n, sum) )
+print("SUM of first {0} numbers is {1}".format(n, sum))
+
+# math formula 
+n1 = int(input('Enter The number '))
+
+sum_res = n1 * (n1 + 1) / 2
+
+print('Sum of first {0} number is {1} '.format(n1, sum_res))
