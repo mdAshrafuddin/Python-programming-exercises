ab2ef60c28dc8b2b910b521626227d94fb5b2730
mdAshrafuddin <mdtanjil01753511918@gmail.com>
2021-11-18 22:43:04 +0600

    area of circle

---
+++ 10-exercise-area-circle.py
@@ -0,0 +1,13 @@
+"""
+Python Program for Program to find area of a circle
+
+Area = pi * r2
+where r is radius of circle 
+
+"""
+
+def area_circle(r):
+    pi = 3.1416
+    return pi * (r*r)
+
+print(area_circle(10))
